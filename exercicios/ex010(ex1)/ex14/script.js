810f49c216a00928fb7b9b335c1a4d58012b4f1c
Isabella-Pompeo <isabellapompeo273@gmail.com>
2024-04-28 23:48:13 -0300

    A

--- exercicios/ex010(ex1)/ex14/script.js
+++ exercicios/ex010(ex1)/ex14/script.js
@@ -3,21 +3,24 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var min =  data.getMinutes()
+    var sec = data.getSeconds()
 
 
     
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} horas, ${min} minutos e ${sec} segundos.`
     if ( hora >= 0 && hora <= 12) {
         //BOM DIA!!
         img.src = 'pexels-torsten-kellermann-955656.jpg'
         document.body.style.background = '#cebd85'
-    } else if ( hora >= 12 && hora <= 18) {
+    } else if ( hora >= 12 && hora <= 17) {
         //BOA TARDE!!
         img.src = 'pexels-george-desipris-858241.jpg'
         document.body.style.background = '#905925'
     } else {
         //BOA NOITE!!
         img.src = 'pexels-pixabay-414144.jpg'
+        
         document.body.style.background = '#1d335f'
     }
 }
